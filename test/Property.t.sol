6709604cc56a65a1c9b867b08f6e32eafe981768
usmanrahim <usmanrahim2000@gmail.com>
2023-08-25 10:40:02 +0500

    Added New Test Cases

--- test/Property.t.sol
+++ test/Property.t.sol
@@ -46,7 +46,73 @@ contract PropertyTest is Test {
         assertEq(mytoken.balanceOf(address(5)), 250);
     }
 
-    function testCreatingNftAndListing() public {
+    // Voting for sale so that owner can sell the Property to any other address
+    function testVotingForSale() public{
+        propertynft.safeMint(address(1), 1);
+        assertEq(propertynft.balanceOf(address(1)), 1);
+        vm.prank(address(1));
+        propertynft.setApprovalForAll(address(mytoken), true);
+        vm.prank(address(1));
+        propertynft.setApprovalForAll(address(propertynft), true);
+        // checking nft listed is true for beforetokentransfer()
+        assertEq(propertynft.getListNft(1), true);
+
+        // listing property to the contract
+        vm.prank(address(1));
+        mytoken.listProperty(1, 1000);
+        assertEq(mytoken.totalSupply(), 1000);
+
+        // address(2) buy 25% of share
+        vm.deal(address(2), 10 ether);
+        vm.prank(address(2));
+        mytoken.buyShare{value: 2.5 ether}(1, 25);
+        assertEq(mytoken.balanceOf(address(2)), 250);
+
+        // voting to sell
+        vm.prank(address(2));
+        mytoken.voteForSale(1);
+        assertEq(mytoken.voters(), 1);
+    }
+
+    // Buying whole property even if any address buy some shares from this
+    function testBuyingWholeProperty() public{
+        propertynft.safeMint(address(1), 1);
+        assertEq(propertynft.balanceOf(address(1)), 1);
+        vm.prank(address(1));
+        propertynft.setApprovalForAll(address(mytoken), true);
+        vm.prank(address(1));
+        propertynft.setApprovalForAll(address(propertynft), true);
+        // checking nft listed is true for beforetokentransfer()
+        assertEq(propertynft.getListNft(1), true);
+
+        // listing property to the contract
+        vm.prank(address(1));
+        mytoken.listProperty(1, 1000);
+        assertEq(mytoken.totalSupply(), 1000);
+
+        // address(2) buy 25% of share
+        vm.deal(address(2), 10 ether);
+        vm.prank(address(2));
+        mytoken.buyShare{value: 2.5 ether}(1, 25);
+        assertEq(mytoken.balanceOf(address(2)), 250);
+
+        // voting to sell
+        vm.prank(address(2));
+        mytoken.voteForSale(1);
+        assertEq(mytoken.voters(), 1);
+
+        // buying whole property
+        vm.deal(address(3), 11 ether);
+        vm.prank(address(3));
+        mytoken.buyWholeProperty{value: 10 ether}(1);
+        assertEq(propertynft.balanceOf(address(1)), 0);
+        assertEq(propertynft.balanceOf(address(3)), 1);
+        assertEq(address(address(3)).balance, 1 ether);
+        assertEq(address(address(mytoken)).balance, 12.5 ether);
+    }
+
+    // Shareholders can redeem their amount
+    function testCreatingNftAndListingAndRedeem() public {
         propertynft.safeMint(address(1), 1);
         assertEq(propertynft.balanceOf(address(1)), 1);
         vm.prank(address(1));
@@ -86,6 +152,7 @@ contract PropertyTest is Test {
         mytoken.redeem(250, 1);
     }
 
+    // Owner of the nft want to sell the whole property without voting for sale
     function testFailTransferWithoutVoting() public {
         propertynft.safeMint(address(1), 1);
         assertEq(propertynft.balanceOf(address(1)), 1);
@@ -112,6 +179,7 @@ contract PropertyTest is Test {
         propertynft.safeTransferFrom(address(1), address(5), 1);
     }
 
+    // Buying share from another share holder
     function testBuyShare_FromShareholder() public {
         propertynft.safeMint(address(1), 1);
         assertEq(propertynft.balanceOf(address(1)), 1);
@@ -142,6 +210,7 @@ contract PropertyTest is Test {
 
     }
 
+    // This test will fail because property has only 100% share 
     function testFail_BuyingSharesMoreThan_100_percent() public{
         propertynft.safeMint(address(1), 1);
         assertEq(propertynft.balanceOf(address(1)), 1);
@@ -169,6 +238,7 @@ contract PropertyTest is Test {
         mytoken.buyShare{value: 8 ether}(1, 80);
     }
 
+    // Any address can buy shares before exceeding to 100%
     function test_Buy_Less_Than_100_percentShare() public{
         propertynft.safeMint(address(1), 1);
         assertEq(propertynft.balanceOf(address(1)), 1);
@@ -205,6 +275,55 @@ contract PropertyTest is Test {
 
     }
 
+    // This function should fail because shares can't be buy more than 100%
+    function testFail_BuyMoreThan100PercentShare() public{
+        propertynft.safeMint(address(1), 1);
+        assertEq(propertynft.balanceOf(address(1)), 1);
+        vm.prank(address(1));
+        propertynft.setApprovalForAll(address(mytoken), true);
+        vm.prank(address(1));
+        propertynft.setApprovalForAll(address(propertynft), true);
+        // checking nft listed is true for beforetokentransfer()
+        assertEq(propertynft.getListNft(1), true);
+
+        // listing property to the contract
+        vm.prank(address(1));
+        mytoken.listProperty(1, 1000);
+        assertEq(mytoken.totalSupply(), 1000);
+
+        // address(2) buy 25% of share
+        vm.deal(address(2), 10 ether);
+        vm.prank(address(2));
+        mytoken.buyShare{value: 2.5 ether}(1, 25);
+        assertEq(mytoken.balanceOf(address(2)), 250);
+
+        // address(3) buy 25%
+        vm.deal(address(3),10 ether);
+        vm.prank(address(3));
+        mytoken.buyShare{value: 2.5 ether}(1, 25);
+        assertEq(mytoken.balanceOf(address(3)), 250);
+
+        // checking remaining balance of address(1) (owner of the token)
+        vm.prank(address(1));
+        assertEq(mytoken.balanceOf(address(1)), 500);
+
+        // checking remaining percentage
+        assertEq(mytoken.remainingPercentage(),50);
+
+        // Address(4) will buy 50% share
+        vm.deal(address(4),10 ether);
+        vm.prank(address(4));
+        mytoken.buyShare{value: 5 ether}(1, 50);
+        assertEq(mytoken.balanceOf(address(4)), 500);
+
+        // Address(5) trying to buy 25% share
+        vm.deal(address(5),10 ether);
+        vm.prank(address(5));
+        mytoken.buyShare{value: 2.5 ether}(1, 25);
+        assertEq(mytoken.balanceOf(address(4)), 250);
+    }
+
+    // Withdrawing funds
     function testWithdraw() public{
         propertynft.safeMint(address(1), 1);
         assertEq(propertynft.balanceOf(address(1)), 1);
